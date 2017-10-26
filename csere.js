abb78608483bd9d7a12de6984c4acb48a78c7d1e
vzsoz <v.zsozsesz@gmail.com>
2017-10-26 17:31:27 +0200

    Initial commit

---
+++ csere.js
@@ -0,0 +1,52 @@
+var x=3;
+var y=7;
+
+function segednelkul(x,y) {
+    x=x+y;
+    y=x-y;
+    x=x-y;
+    console.log("x :"+x+" y :"+y);
+};
+
+function segeddel1(x,y) {
+
+    var temp=x;
+    x=y;
+    y=temp;
+    console.log("x :"+x+" y :"+y);
+};
+
+function segeddel2(x,y) {
+
+    var temp= x+y;
+    y=temp-y;
+    x=temp-x;
+
+    console.log("x :"+x+" y :"+y);
+};
+
+function tombbel(x,y) {
+    var array =[];
+    array.push(x);
+    array.push(y);
+    x=array[1];
+    y=array[0];
+
+    console.log("x :"+x+" y :"+y);
+};
+
+console.log("segédváltozó nélküli megoldás:")
+console.log("x :"+x+" y :"+y);
+segednelkul(x,y);
+
+console.log("\nsegédváltozóval történő megoldás:")
+console.log("x :"+x+" y :"+y);
+segeddel1(x,y);
+
+console.log("\nsegédváltozóval történő megoldás 2:")
+console.log("x :"+x+" y :"+y);
+segeddel2(x,y);
+
+console.log("\ntömb segítségévell történő megoldás 2:")
+console.log("x :"+x+" y :"+y);
+tombbel(x,y);
